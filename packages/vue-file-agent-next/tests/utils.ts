b82c87f5e7befac3327830f3a6e5315df52df4b9
Adrian Sieger <a.sieger@innobis.de>
2022-11-14 16:48:11 +0100

    fix: build (spec was failing but not seperately run tests)
    Release-As: 1.0.2

--- packages/vue-file-agent-next/tests/utils.ts
+++ packages/vue-file-agent-next/tests/utils.ts
@@ -1,37 +1,25 @@
-import type {DefineComponent} from 'vue'
-import { config, shallowMount, VueWrapper } from '@vue/test-utils'
-
-config.global.renderStubDefaultSlot = true
+import {VueWrapper} from '@vue/test-utils'
 
 export const createFile = (name: string, size: number, mimeType: string) => {
-  name = name || 'mock.txt';
-  size = size || 1024;
-  mimeType = mimeType || 'plain/txt';
+  name = name || 'mock.txt'
+  size = size || 1024
+  mimeType = mimeType || 'plain/txt'
 
   const range = (count: number) => {
-    let output = '';
+    let output = ''
     for (let i = 0; i < count; i++) {
-      output += 'a';
+      output += 'a'
     }
-    return output;
-  };
+    return output
+  }
 
-  const blob = new Blob([range(size)], { type: mimeType });
-  (blob as any).lastModifiedDate = new Date();
-  (blob as any).name = name;
+  const blob = new Blob([range(size)], {type: mimeType})
+  ;(blob as any).lastModifiedDate = new Date()
+  ;(blob as any).name = name
 
-  return blob as File;
-};
-
-export const getWrapper = (Component: DefineComponent<any, any, any>, propsData: object): VueWrapper<any> => {
-  return shallowMount(Component, {
-    props: propsData,
-  });
+  return blob as File
 }
 
 export const getText = (wrapper: VueWrapper<any>, selector: string) => {
-  return wrapper
-    .find(selector)
-    .text()
-    .trim();
+  return wrapper.find(selector).text().trim()
 }
